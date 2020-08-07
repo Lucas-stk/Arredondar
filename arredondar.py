9462b1d9d9c3dd777fc2b4e465f16b8174a08ba3
Lucas Silva <69328035+Lucas-stk@users.noreply.github.com>
2020-08-07 11:52:21 -0300

    Update arredondar.py

--- arredondar.py
+++ arredondar.py
@@ -5,6 +5,5 @@ raiz = math.sqrt(num)
 print('A raiz de {} é igual a: {}\n'.format(num, raiz))
 p = int(input('Se deu número com vírgula, informe seu número novamente para ser arredondado: '))
 print('\n')
-print('A raiz arredondada do número {} é igual a: {}\n'.format(num, math.ceil(raiz)))
-
+print('A raiz arredondada do número {} é igual a: {}\n'.format(num, math.floor(raiz)))
 print('##########Feito por Lucas Silva##########')
